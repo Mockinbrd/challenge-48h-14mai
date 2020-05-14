5b0e74fcd7581a35d0346107a68c7ac247d47797
Mockinbrd <marvyn.ap@gmail.com>
2020-05-14 13:27:03 +0200

    Edge Water Version 0.1.5

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,52 @@
-<h1>EH !</h1>
+<head>
+
+    <title>Edge Water</title>
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.css">
+
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Edge Water</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor03">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+<div class="container text-center">
+
+    <h1>ACCUEIL</h1>
+
+
+    <p>This is Edge Water</p>
+
+</div>
 
 
 
 
-<p>This is Edge Water</p>
+
+</body>
+
