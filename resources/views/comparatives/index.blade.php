fee24614dec38c375d742ffb42095f2f2a8096e1
Antonin <antonin.demaneche@ynov.com>
2020-05-15 10:54:03 +0200

    Edge Water O.1.5 - Ajout des cartes comparatives

--- resources/views/comparatives/index.blade.php
+++ resources/views/comparatives/index.blade.php
@@ -1,4 +1,21 @@
 @extends('layouts.default')
 @section('content')
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <style>
+        .selector-for-some-widget {
+        box-sizing: content-box;
+    }  
+        </style>
+    </head>
     <h1>Comparatives</h1>
+    <div>
+        <hr>
+        <h2>Carte comparative des patisier :</h2>
+        <img src="https://cdn.discordapp.com/attachments/517364842659577858/710766195829112902/Carte_patisier.jpg" alt="">
+        <hr>
+        <h2>Carte comparative des cordonniers :</h2>
+        <img src="https://media.discordapp.net/attachments/517364842659577858/710766944940195900/unknown.png?width=731&height=564" alt="">
+    </div>
 @endsection
