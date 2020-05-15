ee4a5b3fb6d7a3f1913df3b7fc9de59c277d1994
Manon <manon.fargues@ynov.com>
2020-05-15 11:45:59 +0200

    Edge Water Version 0.1.4 - Ajout d'un article sponsorisé

--- resources/views/selections/showlequatriememur.blade.php
+++ resources/views/selections/showlequatriememur.blade.php
@@ -118,11 +118,3 @@
 
 </html>
 @endsection
-        <!-- <div class="rating">
-            <h4>!Votre avis nous interresse<h2><!--
-            --><a href="#5" title="Donner 5 étoiles">☆</a><!--
-            --><a href="#4" title="Donner 4 étoiles">☆</a><!--
-            --><a href="#3" title="Donner 3 étoiles">☆</a><!--
-            --><a href="#2" title="Donner 2 étoiles">☆</a><!--
-            --><a href="#1" title="Donner 1 étoile">☆</a>
-            </div> -->
